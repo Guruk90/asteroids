5b9f377affb67fbfc8704e9422cdbf4110b3fcdf
Guruk90 <daniel.i.hansson@gmail.com>
2025-01-10 16:54:59 +0100

    "added asteriods and made them move"

--- player.py
+++ player.py
@@ -38,5 +38,5 @@ class Player(CircleShape):
             self.rotate(dt)
         if keys[pygame.K_w]:
             self.move(dt)
-            if keys[pygame.K_s]:
-                self.move(-dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
